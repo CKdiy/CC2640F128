3e1ea644c8062e0d74d2d92c7f01229a6ca0bbdf
chenkai <1490520478@qq.com>
2019-01-17 17:16:34 +0800

    加入SNV数据存储单元管理文件

---
+++ src/examples/simple_observer/cc26xx/app/snv.c
@@ -0,0 +1,183 @@
+/*
+ * snv.c
+ * Description: nvram managerment for system
+ */
+
+#include <string.h>
+#include "osal_snv.h"
+#include "snv.h"
+#include "tagmgr.h"
+#include "bcomdef.h"
+
+#define ntohs(x) (uint16_t)( ((uint16_t)(x<<8) & 0xFF00) | ((uint16_t)(x>>8) & 0x00FF) )
+
+static const block_info_t fb[] =
+{
+    /* block ID (must < 0xf)    size */
+	{BLE_NVID_DEVINF_START,       9},
+	{BLE_NVID_DRFLG_START,        6},
+};
+
+#define FB_SIZE         (sizeof(fb)/sizeof(block_info_t))
+
+uint8_t nvBuf[10] = {0};
+
+static int Nvram_Block_Init(uint8_t nvid, uint8_t len);
+static int Nvram_Block_Check(uint8_t nvid, uint8_t len);
+static int DevInf_Snv_Init(uint8_t len);
+
+static int Ble_DrFailureNv_Init(uint8_t len);
+
+/* Init nvram */
+void Nvram_Init(void)
+{  
+	 uint8_t i;
+	 
+	 for(i=0; i<FB_SIZE; i++)
+	 {
+    	if (Nvram_Block_Check(fb[i].NVid, fb[i].size) != 0)
+		{
+			Nvram_Block_Init(fb[i].NVid, fb[i].size);
+		}
+	 }
+}
+
+static int Nvram_Block_Check(uint8_t nvid, uint8_t len)
+{
+    uint32_t	crc = 0;
+	uint8_t		status;
+	
+    if(nvid > BLE_NVID_CUST_END)
+	 	return -1;
+	
+	memset(nvBuf, 0, sizeof(nvBuf));
+	
+	status = osal_snv_read(nvid, len, (void *)nvBuf);
+	if(status != SUCCESS)
+	 	return -1;
+	
+	memcpy((void *)&crc, nvBuf, sizeof(crc));
+    if (crc != crc32(0, &nvBuf[sizeof(crc)], len - sizeof(crc)))
+        return -1;
+    
+    return 0;
+}
+
+static int Nvram_Block_Init(uint8_t nvid, uint8_t len)
+{	
+	if(nvid > BLE_NVID_CUST_END)
+	 	return -1;
+	
+	if(BLE_NVID_DEVINF_START == nvid)
+	{
+		return DevInf_Snv_Init(len);
+	}
+	else if(BLE_NVID_DRFLG_START == nvid)
+	{
+	 	return Ble_DrFailureNv_Init(len); 
+	}
+	 
+    return 0;
+}
+
+static int DevInf_Snv_Init(uint8_t len)
+{
+ 	uint8_t		status;
+	snv_device_inf_t *ptr = (snv_device_inf_t *)nvBuf;
+	
+	memset(nvBuf, 0, sizeof(nvBuf));
+	/* LORA Para */
+	ptr->loraPara_u.bit_t.power			= 1;
+	ptr->loraPara_u.bit_t.rate  		= 1;
+	ptr->loraPara_u.bit_t.channel	 	= 1;
+	
+	/* TAG Para */
+	ptr->tagPara_u.bit_t.sos            = 0;
+	ptr->tagPara_u.bit_t.sleep_delay    = 0;
+	ptr->tagPara_u.bit_t.scan_num       = 3;
+	ptr->tagPara_u.bit_t.res            = 0;
+	ptr->tagPara_u.bit_t.up_interval    = 3;
+	ptr->tagPara_u.bit_t.up_interval_s  = 8;
+	ptr->tagPara_u.tagPara              = ntohs(ptr->tagPara_u.tagPara);
+	
+	/* BLE Para */
+	ptr->blePara_u.bit_t.res            = 0;
+	ptr->blePara_u.bit_t.scan_time      = 5;
+	
+	ptr->crc32 = crc32(0, (uint8_t *)(&ptr->loraPara_u), len - sizeof(uint32_t));
+	
+	status = osal_snv_write(BLE_NVID_DEVINF_START, len,  ptr);
+	if(status != SUCCESS)
+		return -1;
+	
+	return 0;
+}
+
+static int Ble_DrFailureNv_Init(uint8_t len)
+{
+ 	uint8_t		status;
+	snv_driverfailure_t *ptr = (snv_driverfailure_t *)nvBuf;  
+	
+	memset(nvBuf, 0, sizeof(nvBuf));
+	ptr->lora   = 0;
+	ptr->mems   = 0;
+	
+	ptr->crc32 = crc32(0, (uint8_t *)(&ptr->mems), len - sizeof(uint32_t));
+	
+	status = osal_snv_write(BLE_NVID_DRFLG_START, len,  ptr);
+	if(status != SUCCESS)
+		return -1;
+	
+	return 0;
+}
+
+int Ble_ReadNv_Inf(uint8_t nvid, uint8_t *readbuf)
+{
+	uint8_t		status;
+	uint8_t     i;
+	uint8_t     len;
+	
+	if(nvid > BLE_NVID_CUST_END)
+		return -1;
+	
+	for(i=0; i<FB_SIZE; i++)
+	{
+		if(fb[i].NVid == nvid)
+			len =  fb[i].size; 
+	}
+	
+	status = osal_snv_read(nvid, len, (void *)readbuf);	
+	if(status != SUCCESS)
+		return -1;
+	
+	return 0;
+}
+
+int Ble_WriteNv_Inf(uint8_t nvid, uint8_t *writebuf)
+{
+	uint8_t		status;
+	uint8_t     i;
+	uint8_t     len;
+	uint32_t	crc = 0;    
+	
+	if(nvid > BLE_NVID_CUST_END)
+		return -1;
+	
+	for(i=0; i<FB_SIZE; i++)
+	{
+		if(fb[i].NVid == nvid)
+			len =  fb[i].size; 
+	}
+	
+	memset(nvBuf, 0, sizeof(nvBuf));
+	
+	crc = crc32(0, writebuf, len - sizeof(crc));
+	memcpy(nvBuf, (void *)&crc, sizeof(crc));
+	memcpy(nvBuf + sizeof(crc), writebuf, len - sizeof(crc));
+	  
+	status = osal_snv_write(nvid, len, (void *)nvBuf);	
+	if(status != SUCCESS)
+		return -1;
+	
+	return 0;
+}
--- src/examples/simple_observer/cc26xx/app/tagmgr.h
+++ src/examples/simple_observer/cc26xx/app/tagmgr.h
@@ -30,15 +30,89 @@
 /*********************************************************************
  * TYPEDEFS
  */
+typedef union 
+{
+  	struct
+	{
+	  	uint8_t		pkt_len    		:6;      // Bit5~ Bit0  Payload length
+		uint8_t		pkt_type		:2;      // Bit7~ Bit6  Package type     
+    }bit_t; 
+	
+	uint8_t payLoadInf;
+}payload_inf_n;
+
+/**************************************
+ * deviceinf union
+ */
+typedef union 
+{
+	struct
+	{		
+		uint8_t     beaconNum_1		:2;      // Bit2~ Bit0
+		uint8_t     beaconNum_2     :2;      // Bit5~ Bit3
+		uint8_t     beaconNum_3     :2;      // Bit8 ~Bit6
+		uint8_t     beaconNum_4     :2;      // Bit11~ Bit9
+		uint8_t     res    			:2;      // Bit13~ Bit12    reserve
+		uint8_t     sos    			:1;      // Bit14   sos
+		uint8_t     vbat    		:1;      // Bit15  Low voltage alarm
+	}bit_t;
+
+	uint16_t	tagPacketInf;
+}device_up_inf_n;
 
 /**
- * taginfo Structure
+ *  lora union
+ */
+typedef union lora_para_n
+{
+	struct
+	{
+		uint8_t		power   		:8;      // Bit15~Bit8 Lora Power
+		uint8_t     channel         :5;      // Bit4~Bit0  channel
+		uint8_t     rate            :3;      // Bit7~Bit5  rate
+	}bit_t;
+	
+	uint16_t    loraPara;
+}lora_para_n;
+
+/**
+ *  tag union
+ */
+typedef union 
+{
+	struct
+	{ 	    
+		uint8_t		up_interval_s	:4;      // Bit3~Bit0 
+		uint8_t     up_interval     :4;      // Bit7~Bit4    Reporting interval 
+		uint8_t     res             :3;      // Bit10~Bit8   reserve
+		uint8_t     scan_num        :2;      // Bit12~Bit11  scan times
+		uint8_t     sleep_delay     :2;      // Bit14~Bit13 
+		uint8_t		sos             :1;      // Bit15 sos: 0  enable 1 disable 
+	}bit_t;
+	
+	uint16_t    tagPara;
+}tag_para_n;
+
+/**
+ *  Ble parameter
+ */
+typedef union 
+{
+	struct
+	{
+		uint8_t		scan_time		:4;       // Bit3~Bit0   
+		uint8_t		res	      		:4;       // Bit7~Bit4   reserve      
+	}bit_t;
+	
+    uint8_t		blePara;
+}ble_para_n;
+
+/**
+ * taginfo union
  */
 typedef struct
 {
-   	uint16_t tagIndex;      //tag接收序列
-	uint8_t rssi;	
-	uint8_t major[2];	     	
+	uint8_t rssi;	     	
 	uint8_t minor[2];
 }tagInfStruct;
 
@@ -50,7 +124,7 @@ typedef struct
 	tagInfStruct *tagInfBuf_t;  //缓存收到的所有tag
 	uint16_t  index;	      
 	uint8_t   tagNum;        //tagNum <= 8  
-}observerInfStruct;
+}observerInfStruct; 
 
 /**
  *  Userinf Structure
@@ -63,6 +137,27 @@ typedef struct
     uint8_t  txTagNum;          // txTagNum <= 4  上传Tag数量
 	tagInfStruct *tagInfBuf_t;  // 单次上传的Tag信息
 }userTxStruct;
+
+typedef struct
+{
+	uint8_t           pre;
+	payload_inf_n     payload_inf; 
+}loratag_pkt_hdr_t;
+
+typedef struct
+{
+	uint32_t   			crc32;
+	lora_para_n			loraPara_u;
+	tag_para_n			tagPara_u;
+	ble_para_n    		blePara_u;
+}snv_device_inf_t;
+
+typedef struct
+{
+    uint32_t    crc32;
+    uint8_t     mems;
+    uint8_t     lora;
+}snv_driverfailure_t;
 
 /*********************************************************************
  * VARIABLES
